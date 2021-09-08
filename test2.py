d4f997e8f82516cbd2dd53ee2963b53114683aec
Zansan-bit <84367201+Zansan-bit@users.noreply.github.com>
2021-09-09 08:54:08 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,7 +1,11 @@
 # exam
 def  mail(input_string):
-	if input_string == 'yt@gmail.com':
-		print('承認されました')
+	if input_string == '@':
+
+		if input_string == '.com'
+			print('承認されました')
+		else :
+			print('何かがおかしいです')
 	else :
 		print('何かがおかしいです')
 
